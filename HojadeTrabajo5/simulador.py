371b3b34ad64d5f7465bd0deefff5b6af3508ae2
Andrei-Portales <30614183+Andrei-Portales@users.noreply.github.com>
2020-02-29 19:54:20 -0600

    Update simulador.py

--- HojadeTrabajo5/simulador.py
+++ HojadeTrabajo5/simulador.py
@@ -1,43 +1,68 @@
 import random
 import simpy
+import statistics
+import matplotlib.pyplot as plt
 
-# variables dinamicas
-cantidaddeProcesos = 5
-numProcesos = 3
-numRam = 100
+# 25 procesos, luego con 50 procesos, con 100, 150 y 200
 
-# variables de simulador
-random.seed(10)  # fijar el inicio de random
-tiempototal = 0
-env = simpy.Environment()
-RAM = simpy.Container(env, init=numRam, capacity=numRam)
-CPU = simpy.Resource(env, capacity=numProcesos)
+procesosEjecutar = [25,50,100,150,200]
+tiempoTotales = []
 
+for proceso in procesosEjecutar:
+    
+    for _ in range(10):
+        print("")
+    
+    # variables dinamicas
+    cantidaddeProcesos = proceso
+    numProcesos = 3
+    numRam = 100
 
-# funcion que simula un proceso
-def process(name, tiempoEspera, environment, cpu, ram):
-    global tiempototal
-    yield environment.timeout(tiempoEspera)
-    tiempoInicio = environment.now
-    memoriaUtilizar = random.randint(1, 10)
-    ram.get(memoriaUtilizar)
-    print(ram.level)
-    cantidadProcesos = random.randint(1, 10)
-    print(name, "se almaceno en la memoria en", tiempoInicio, "y tiene", cantidadProcesos, "procesos")
+    # variables de simulador
+    tiempos = []
+    random.seed(10)  # fijar el inicio de random
 
-    with cpu.request() as turno:
-        yield turno
-        yield environment.timeout(cantidadProcesos)
-        print(name, "sale del CPU a las ", environment.now)
-    ram.put(memoriaUtilizar)
-    tiempoProceso = env.now - tiempoInicio
-    print(name, "se tardo", tiempoProceso)
-    tiempototal += tiempoProceso
+    env = simpy.Environment()
+    RAM = simpy.Container(env, init=numRam, capacity=numRam)
+    CPU = simpy.Resource(env, capacity=numProcesos)
 
 
-for i in range(cantidaddeProcesos):
-    env.process(process("Proceso" + str(i + 1), random.expovariate(1.0/10), env, CPU, RAM))
+    # funcion que simula un proceso
+    def process(name, tiempoEspera, environment, cpu, ram):
+        global tiempototal
+        yield environment.timeout(tiempoEspera)
+        tiempoInicio = environment.now
+        memoriaUtilizar = random.randint(1, 10)
+        ram.get(memoriaUtilizar)
+        cantidadProcesos = random.randint(1, 10)
+        print(name, "se almaceno en la memoria en", tiempoInicio, "y tiene", cantidadProcesos, "procesos")
 
-env.run(until=cantidaddeProcesos*10)
+        with cpu.request() as turno:
+            yield turno
+            yield environment.timeout(cantidadProcesos)
+            print(name, "sale del CPU a las ", environment.now)
+        ram.put(memoriaUtilizar)
+        tiempoProceso = env.now - tiempoInicio
+        tiempos.append(tiempoProceso)
+        print(name, "se tardo", tiempoProceso)
 
-print("\n\n\nTiempo promedio de ejecucion de", cantidaddeProcesos, "procesos: ", tiempototal / cantidaddeProcesos)
+    
+    for i in range(cantidaddeProcesos):
+        env.process(process("Proceso" + str(i + 1), random.expovariate(1.0 / 10), env, CPU, RAM))
+        
+
+    env.run(until=cantidaddeProcesos * 10)
+
+    print("\n\n\nTiempo promedio de ejecucion de", cantidaddeProcesos, "procesos: ", sum(tiempos) / cantidaddeProcesos)
+    print("La desviacion estandar es de:", statistics.stdev(tiempos))
+    tiempoTotales.append(sum(tiempos) / cantidaddeProcesos)
+
+
+def grafica(x,y):
+    plt.plot(x,y)
+    plt.xlabel('Numero de Procesos')
+    plt.ylabel('Tiempo Promedio')
+    plt.title('Graficas de Simulador de Procesos')
+    plt.show()
+
+grafica(procesosEjecutar,tiempoTotales)
